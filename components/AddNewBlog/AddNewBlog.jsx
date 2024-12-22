79337b5373715e2cf31dbc8e68c831168b5d9d90
Laxman67 <laxmanhenricks94@gmail.com>
2024-12-22 13:56:01 +0530

    addNewBlog

--- components/AddNewBlog/AddNewBlog.jsx
+++ components/AddNewBlog/AddNewBlog.jsx
@@ -1,21 +1,30 @@
-import { Dialog, DialogContent, DialogFooter } from '@radix-ui/react-dialog';
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogTitle,
+} from '@/components/ui/dialog';
 
 import React, { Fragment, useState } from 'react';
 import { Button } from '../ui/button';
 import { Input } from '../ui/input';
 import { Label } from '../ui/label';
 
-const AddNewBlog = ({ openBlogDialog, setOpenBlogDialog }) => {
-  const [title, setTitle] = useState('');
-  const [description, setDescription] = useState('');
-
+const AddNewBlog = ({
+  openBlogDialog,
+  setOpenBlogDialog,
+  blogFormData,
+  setBlogFormData,
+  handleSaveBlogData,
+}) => {
   return (
-    <>
-      <Button onClick={() => setOpenBlogDialog(true)}>
+    <div className="flex flex-col my-3 mx-3">
+      <Button onClick={() => setOpenBlogDialog(true)} className="w-[200px]  ">
         Add New Blog Section
       </Button>
       <Dialog open={openBlogDialog} onOpenChange={setOpenBlogDialog}>
         <DialogContent className="sm:max-w-[425px]">
+          <DialogTitle></DialogTitle>
           <div className="grid gap-4 py-4">
             <div className="grid grid-cols-4 items-center gap-4">
               <Label htmlFor="name" className="text-right">
@@ -23,8 +32,11 @@ const AddNewBlog = ({ openBlogDialog, setOpenBlogDialog }) => {
               </Label>
               <Input
                 id="title"
+                value={blogFormData.title}
                 className="col-span-3"
-                onChane={(e) => setTitle(e.target.value)}
+                onChange={(e) =>
+                  setBlogFormData({ ...FormData, title: e.target.value })
+                }
               />
             </div>
             <div className="grid grid-cols-4 items-center gap-4">
@@ -33,17 +45,22 @@ const AddNewBlog = ({ openBlogDialog, setOpenBlogDialog }) => {
               </Label>
               <Input
                 id="description"
+                value={blogFormData.description}
                 className="col-span-3"
-                onChane={(e) => setDescription(e.target.value)}
+                onChange={(e) =>
+                  setBlogFormData({ ...FormData, description: e.target.value })
+                }
               />
             </div>
           </div>
           <DialogFooter>
-            <Button type="button">Save changes</Button>
+            <Button type="button" onClick={handleSaveBlogData}>
+              Save changes
+            </Button>
           </DialogFooter>
         </DialogContent>
       </Dialog>
-    </>
+    </div>
   );
 };
 
--- components/BlogOverview/BlogOverview.jsx
+++ components/BlogOverview/BlogOverview.jsx
@@ -3,14 +3,40 @@ import React, { useState } from 'react';
 
 import AddNewBlog from '../AddNewBlog/AddNewBlog';
 
+const initailBlogFormData = {
+  title: '',
+  description: '',
+};
+
 const BlogOverview = () => {
   const [openBlogDialog, setOpenBlogDialog] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [blogFormData, setBlogFormData] = useState(initailBlogFormData);
+
+  const handleSaveBlogData = async () => {
+    try {
+      const apiResponse = await fetch('api/add-blog', {
+        method: 'POST',
+        body: JSON.stringify(blogFormData),
+      });
+      const result = apiResponse.json();
+      console.log(result);
+    } catch (error) {
+      console.log('error Occured ', error);
+      setLoading(false);
+      setBlogFormData(initailBlogFormData);
+    }
+  };
 
   return (
     <div className=" min-h-screen flex flex-col gap-10 bg-gradient-to-r from-purple-400 to-blue-600  ">
       <AddNewBlog
         openBlogDialog={openBlogDialog}
         setOpenBlogDialog={setOpenBlogDialog}
+        loading={loading}
+        blogFormData={blogFormData}
+        setBlogFormData={setBlogFormData}
+        handleSaveBlogData={handleSaveBlogData}
       />
 
       <div className="">Blog List Section</div>
